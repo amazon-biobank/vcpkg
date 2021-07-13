30470256361e4aab3f6644c4b6305425885c99be
90felipe09 <felhipeshiraishi@gmail.com>
2021-07-12 22:25:59 -0300

    [lyra2-file-encryptor]: upgrades port to 1.3.0

--- ports/lyra2-file-encryptor/portfile.cmake
+++ ports/lyra2-file-encryptor/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO amazon-biobank/lyra2-file-encryptor
-    REF e5cf962633f6a0a7acd3f3adf4fcbf10f014e6bf
-    SHA512 e750d5530cd5c14f2dc61f5abf8ee1e34884c12d5f39e0cbad623aa72a842efaa9b2fd0728ed64ee4bc6f2b0299dcea9e6ea23f59cb1826e1577cdf5158e8009    
-    HEAD_REF V_1_2
+    REF 3a4906d06e6075cbaad984728c40e1f3a3260411
+    SHA512 84d0e0070c0570421fdd18f39ce270adeab45117b78d2340fe9c8f0b352381aa1574b0aca50bf0a3632a3f54af41bad4d24c68dd3b9ea82d4af64676cce44b2d    
+    HEAD_REF v_1_3
 )
 
 vcpkg_configure_cmake(
